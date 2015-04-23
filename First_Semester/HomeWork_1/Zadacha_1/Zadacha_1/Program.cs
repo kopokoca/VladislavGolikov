19bfdf6b643d37a0acab286b8793f774d728f77b
kopokoca <golikovlad@yandex.ru>
2015-04-23 22:34:00 +0300

    commit

---
+++ First_Semester/HomeWork_1/Zadacha_1/Zadacha_1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadacha_1
+{
+    class Factorial
+    {
+        public static int factorial(int number)
+        {
+            if (number < 0)
+            {
+                return 0;
+            }
+            else
+            {
+                int result = 1;
+                for (int i = number; i > 1; --i)
+                {
+                    result = result * i;
+                }
+                return result;
+            }
+           
+        }
+
+        public static void Main()
+        {
+            Console.WriteLine("Введите число: ");
+            int number = int.Parse(Console.ReadLine());
+            int result = factorial(number);
+            if (result != 0)
+            {
+                Console.WriteLine("Факториал числа: " + result);
+            }
+            else
+            {
+                Console.WriteLine("Число должно быть неотрицательным!");
+            }
+           
+        }
+    }
+}
