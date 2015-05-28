73767f7e48c2958c5755fc33c2f6c1c7fdba461c
kopokoca <golikovlad@yandex.ru>
2015-05-29 01:14:24 +0300

    delete other files

---
+++ Task1.2/Zadacha_2/Zadacha_2.Tests/Zadacha_2Test.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Zadacha_2.Tests
+{
+    [TestClass]
+    public class Zadacha_2Test
+    {
+        [TestMethod]
+        public void fibTest1()
+        {
+            string result = "1 1 2 3 ";
+            Assert.AreEqual(result, Zadacha_2.Fib.fib(4));
+        }
+
+        [TestMethod]
+        public void fibTest2()
+        {
+            string result = "Ошибка!";
+            Assert.AreEqual(result, Zadacha_2.Fib.fib(0));
+        }
+    }
+}
