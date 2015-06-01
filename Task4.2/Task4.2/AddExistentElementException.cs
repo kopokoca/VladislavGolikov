c02e2c36d09a935a4c2ce447358308b44835edfb
kopokoca <golikovlad@yandex.ru>
2015-06-01 05:44:26 +0300

    task4.2

---
+++ Task4.2/Task4.2/AddExistentElementException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4_2
+{
+    [Serializable]
+    public class AddExistentElementException : Exception
+    {
+        public AddExistentElementException() { }
+        public AddExistentElementException(string message) : base(message) { }
+        public AddExistentElementException(string message, Exception inner) : base(message, inner) { }
+        protected AddExistentElementException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context)
+            : base(info, context) { }
+    }
+}
---
+++ Task4.2/Task4_2.Tests/UnitTest1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Task4_2.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private UniqueList list;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            list = new UniqueList();
+        }
+
+        [TestMethod]
+        public void AddElementsTest()
+        {
+            list.AddListElement(5);
+            Assert.IsTrue(list.IsContains(5));
+            list.AddListElement(7);
+            Assert.AreEqual(2, list.Count());
+            Assert.IsTrue(list.IsContains(7));
+        }
+
+        [TestMethod]
+        public void RemoveElementsTest()
+        {
+            list.AddListElement(4);
+            list.AddListElement(3);
+            list.AddListElement(5);
+            Assert.AreEqual(3, list.Count());
+            Assert.IsTrue(list.RemoveListElement(3));
+            Assert.IsFalse(list.IsContains(3));
+            Assert.IsTrue(list.IsContains(4));
+            Assert.IsTrue(list.IsContains(5));
+            Assert.IsTrue(list.RemoveListElement(5));
+            Assert.AreEqual(1, list.Count());
+            Assert.IsFalse(list.IsContains(5));
+            Assert.IsTrue(list.IsContains(4));
+            Assert.IsTrue(list.RemoveListElement(4));
+            Assert.IsFalse(list.IsContains(4));
+            Assert.AreEqual(0, list.Count());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(RemoveNonexistentElementException))]
+        public void RemoveElements()
+        {
+            list.AddListElement(4);
+            list.RemoveListElement(5);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(AddExistentElementException))]
+        public void AddElements()
+        {
+            list.AddListElement(4);
+            list.AddListElement(4);
+        }
+    }
+}
