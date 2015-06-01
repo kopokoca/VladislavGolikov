7544884bb4bc6c055f48a0b846ebbadd8f582ac3
kopokoca <golikovlad@yandex.ru>
2015-06-01 15:52:53 +0300

    deleted trash

---
+++ Task1.2/Zadacha_2/Zadacha_2.Tests/Zadacha_2Test.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Zadacha_2.Tests
+{
+    [TestClass]
+    public class Zadacha_2Test
+    {
+        [TestMethod]
+        public void fibTest1()
+        {
+            string result = "1 1 2 3 ";
+            Assert.AreEqual(result, Zadacha_2.Fibonacci.Fib(4));
+        }
+
+        [TestMethod]
+        public void fibTest2()
+        {
+            string result = "1 1 2 3 5 8 ";
+            Assert.AreEqual(result, Zadacha_2.Fibonacci.Fib(6));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NegativeNumberException))]
+        public void NegativeNumberTest()
+        {
+            Fibonacci.Fib(-2);
+        }
+    }
+}
