fd9237e168c89c918c4c4073397f10e4ad94bbfd
kopokoca <golikovlad@yandex.ru>
2015-06-01 03:11:32 +0300

    fixed style guide

--- Task1.1/Zadacha_1/Zadacha_1.Tests/UnitTest1.cs
+++ Task1.1/Zadacha_1/Zadacha_1.Tests/UnitTest1.cs
@@ -10,21 +10,21 @@ namespace Zadacha_1.Tests
         public void factorialTest1()
         {
             int result = 120;
-            Assert.AreEqual(result, Zadacha_1.Factorial.factorial(5));
+            Assert.AreEqual(result, Zadacha_1.MySpace.Factorial(5));
         }
 
         [TestMethod]
         public void factorialTest2()
         {
             int result = 5040;
-            Assert.AreEqual(result, Zadacha_1.Factorial.factorial(7));
+            Assert.AreEqual(result, Zadacha_1.MySpace.Factorial(7));
         }
 
         [TestMethod]
         [ExpectedException(typeof(NegativeNumberException))]
         public void NegativeNumberTest()
         {
-            Factorial.factorial(-2);
+            MySpace.Factorial(-2);
         }
     }
 }
--- Task1.1/Zadacha_1/Zadacha_1/Program.cs
+++ Task1.1/Zadacha_1/Zadacha_1/Program.cs
@@ -6,25 +6,16 @@ using System.Threading.Tasks;
 
 namespace Zadacha_1
 {
-    [Serializable]
-    public class NegativeNumberException : Exception
-    {
-        public NegativeNumberException() { }
-        public NegativeNumberException(string message) : base(message) { }
-        public NegativeNumberException(string message, Exception inner) : base(message, inner) { }
-        protected NegativeNumberException(
-          System.Runtime.Serialization.SerializationInfo info,
-          System.Runtime.Serialization.StreamingContext context)
-            : base(info, context) { }
-    }
-
     /// <summary>
-    /// Этот метод считает факториал числа.
+    /// Этот класс считает факториал числа.
     /// </summary>
-    /// <returns>Подсчитанный факториал</returns>
-    public class Factorial
+    public class MySpace
     {
-        public static int factorial(int number)
+        /// <summary>
+        /// Этот метод считает факториал числа.
+        /// </summary>
+        /// <returns>Подсчитанный факториал</returns>
+        public static int Factorial(int number)
         {
             if (number < 0)
             {
@@ -46,7 +37,7 @@ namespace Zadacha_1
         {
             Console.WriteLine("Введите число: ");
             int number = int.Parse(Console.ReadLine());
-            int result = factorial(number);
+            int result = Factorial(number);
             if (result != 0)
             {
                 Console.WriteLine("Факториал числа: " + result);
