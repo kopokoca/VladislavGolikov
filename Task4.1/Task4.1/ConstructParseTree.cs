a351362d457379b9fb61eade9eb3a013429a529d
kopokoca <golikovlad@yandex.ru>
2015-06-01 07:57:05 +0300

    task4.1

---
+++ Task4.1/Task4.1/ConstructParseTree.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4_1
+{
+    public class ConstructParseTree
+    {
+        private ParseTreeNode tree; // корень дерева
+
+        /// <summary>
+        /// Возвращает новый узел оператора symbol
+        /// </summary>
+        private ParseTreeNode Operator(char symbol)
+        {
+            switch (symbol)
+            {
+                case '+': return new Addition();
+                case '-': return new Subtraction();
+                case '*': return new Multiplication();
+                case '/': return new Division();
+            }
+
+            throw new InvalidOperationException("Неправильный символ");
+        }
+
+        /// <summary>
+        /// Добавление элемента в дерево
+        /// </summary>
+        public void AddElement(char value)
+        {
+            if (tree != null)
+            {
+                if (IsOperator(value))
+                {
+                    tree.AddElement(Operator(value));
+                }
+                else
+                {
+                    tree.AddElement(new Operand(value));
+                }
+            }
+            else 
+            {
+                if (IsOperator(value))
+                {
+                    tree = Operator(value);
+                }
+                else
+                {
+                    tree = new Operand(value);
+                }
+            }
+        } 
+
+        /// <summary>
+        /// Вычисление значения арифметического выражения
+        /// </summary>
+        public int CalculateTree()
+        {
+            return tree.Calculate();
+        }
+
+        /// <summary>
+        /// Вывод дерева на экран
+        /// </summary>
+        public string PrintParseTree()
+        {
+            return tree.PrintTree();
+        }
+
+        /// <summary>
+        /// Показывает, является ли указанный символом арифметическим знаком
+        /// </summary>
+        public bool IsOperator(char symbol)
+        {
+            return (symbol == '+') || (symbol == '-') || (symbol == '*') || (symbol == '/');
+        }
+    }
+}
---
+++ Task4.1/Task4.1/Division.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4_1
+{
+    /// <summary>
+    /// Класс для деления
+    /// </summary>
+    class Division : Operator
+    {
+        public override int Calculate()
+        {
+            if (RightChild.Calculate() == 0)
+            {
+                throw new DivideByZero("Деление на ноль");
+            }
+
+            return LeftChild.Calculate() / RightChild.Calculate();
+        }
+
+        public override string Print()
+        {
+            return " /";
+        }
+    }
+}
