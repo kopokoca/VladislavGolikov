b7ad7d5c68a9ddbff4e7cee6edab9e758d9ff4d5
kopokoca <golikovlad@yandex.ru>
2015-06-01 03:28:47 +0300

    fixed style guide

---
+++ Task1.2/Zadacha_2/Zadacha_2/NegativeNumberExeption.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadacha_2
+{
+    /// <summary>
+    /// Класс появляется при вводе отрицательного числа
+    /// </summary>
+    [Serializable]
+    public class NegativeNumberException : Exception
+    {
+        public NegativeNumberException() { }
+        public NegativeNumberException(string message) : base(message) { }
+        public NegativeNumberException(string message, Exception inner) : base(message, inner) { }
+        protected NegativeNumberException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context)
+            : base(info, context) { }
+    }
+}
