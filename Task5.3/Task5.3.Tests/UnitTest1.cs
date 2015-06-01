38e3274caef9a895ab5809823154449d727f227c
kopokoca <golikovlad@yandex.ru>
2015-06-01 06:48:20 +0300

    task5.3

---
+++ Task5.3/Task5.3.Tests/UnitTest1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Task5._3.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private List<int> list1;
+        private List<int> list2;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            list1 = new List<int> { 6, 1, 9 };
+            list2 = new List<int> { 1, 3, 3, 7 };
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int fold = new FoldClass<int>().Fold(list1, 3, (x, y) => x * y);
+            Assert.AreEqual(fold, 3 * list1[0] * list1[1] * list1[2]);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            int fold = new FoldClass<int>().Fold(list2, 7, (x, y) => x * y);
+            Assert.IsTrue(fold == 7 * list2[0] * list2[1] * list2[2] * list2[3]);
+        }
+    }
+}
---
+++ Task5.3/Task5.3/Fold.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5._3
+{
+    /// <summary>
+    /// Класс с реализацией функции Fold
+    /// </summary>
+    public class FoldClass<T>
+    {
+        /// <summary>
+        /// Функция берет список, текущие элемент и функцию
+        /// </summary>
+        /// <returns>Возвращает новое накопленное значение</returns>
+        public T Fold(List<T> list, T value, Func<T, T, T> function)
+        {
+            List<int> foldList = new List<int>();
+
+            foreach (var temp in list)
+            {
+                value = function(value, temp);
+            }
+
+            return value;
+        }
+    }
+}
