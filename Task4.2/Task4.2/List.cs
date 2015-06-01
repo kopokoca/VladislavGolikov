5b59c644a8f798c2f89869c0f6ef89550cad9be7
kopokoca <golikovlad@yandex.ru>
2015-06-01 07:58:22 +0300

    task4.2

---
+++ Task4.2/Task4.2/List.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4_2
+{
+    public class List
+    {
+        public class ListElement
+        {
+            public int Value { get; private set; }
+            public ListElement Next { get; set; }
+
+            public ListElement(int value, ListElement next)
+            {
+                Next = next;
+                Value = value;
+            }
+        }
+
+        private ListElement head;
+        private int size;
+
+        /// <summary>
+        /// Добавление элемента в список
+        /// </summary>
+        public virtual void AddToListElement(int value)
+        {
+            ListElement newElement = new ListElement(value, head);
+            head = newElement;
+            ++size;
+        }
+
+        /// <summary>
+        /// Возвращает размер списка
+        /// </summary>
+        public int Count()
+        {
+            return size;
+        }
+
+        /// <summary>
+        /// Проверка на существование заданного элемента в списке
+        /// </summary>
+        public bool IsContains(int value)
+        {
+            if (head == null)
+            {
+                return false;
+            }
+
+            ListElement current = head;
+
+            while (current.Next != null && current.Value != value)
+            {
+                current = current.Next;
+            }
+
+            return current.Value == value;
+        }
+
+        /// <summary>
+        /// Удаление заданно элемента
+        /// </summary>
+        public virtual bool RemoveListElement(int value)
+        {
+            ListElement previous = head;
+            ListElement current = head;
+
+            while (current != null && current.Value != value)
+            {
+                previous = current;
+                current = current.Next;
+            }
+
+            if (current != null)
+            {
+                if (current == head)
+                {
+                    head = head.Next;
+                }
+                else
+                {
+                    previous.Next = current.Next;
+                }
+
+                size--;
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Task4.2/Task4.2/RemoveNonexistentElementException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4_2
+{
+    [Serializable]
+    public class RemoveNonexistentElementException : Exception
+    {
+        public RemoveNonexistentElementException() { }
+        public RemoveNonexistentElementException(string message) : base(message) { }
+        public RemoveNonexistentElementException(string message, Exception inner) : base(message, inner) { }
+        protected RemoveNonexistentElementException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context)
+            : base(info, context) { }
+    }
+}
