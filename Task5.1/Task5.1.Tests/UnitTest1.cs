f46aefed0c68bbac797a828d3c645b1782d1ad52
kopokoca <golikovlad@yandex.ru>
2015-06-01 08:01:26 +0300

    task5.1

---
+++ Task5.1/Task5.1.Tests/UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Task5._1.Tests
+{
+    [TestClass]
+    public class MapTest
+    {
+        private List<int> list;
+        private List<int> map1;
+        private List<int> map2;
+
+        [TestMethod]
+        public void MapTest1()
+        {
+            list = new List<int> { 1, 8, 6, 3 };
+            map1 = new MapClass<int>().Map(list, x => x * 2);
+            Assert.AreEqual(list[0] * 2, map1[0]);
+            Assert.AreEqual(list[1] * 2, map1[1]);
+            Assert.AreEqual(list[2] * 2, map1[2]);
+            Assert.AreEqual(list[3] * 2, map1[3]);
+        }
+
+        [TestMethod]
+        public void MapTest2()
+        {
+            list = new List<int> { 1, 4, 7, 5 };
+            map2 = new MapClass<int>().Map(list, x => x * x);
+            Assert.AreEqual(list[0] * list[0], map2[0]);
+            Assert.AreEqual(list[1] * list[1], map2[1]);
+            Assert.AreEqual(list[2] * list[2], map2[2]);
+            Assert.AreEqual(list[3] * list[3], map2[3]);
+        }
+    }
+}
